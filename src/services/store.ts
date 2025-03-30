2284f115a65d76317cc012e39327cabbf74c49b0
Said Alistanov <alistanov.said@yandex.ru>
2025-03-30 13:35:04 +0300

    fixed an error with page reloading

--- src/services/store.ts
+++ src/services/store.ts
@@ -16,6 +16,29 @@ import userOrdersReducer from './slices/userOrderSlice';
 
 import { wsUserOrdersMiddleware } from './middleware/userOrderMiddleware';
 
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('reduxState');
+    if (serializedState) {
+      return JSON.parse(serializedState);
+    }
+    return undefined;
+  } catch (err) {
+    return console.error(err);
+  }
+};
+
+const saveState = (state: any) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('reduxState', serializedState);
+  } catch (err) {
+    console.error('Не удалось сохранить', err);
+  }
+};
+
+const persistedState = loadState();
+
 export const rootReducer = combineReducers({
   auth: authReducer,
   ingredients: ingredientsReducer,
@@ -29,7 +52,12 @@ const store = configureStore({
   reducer: rootReducer,
   devTools: process.env.NODE_ENV !== 'production',
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(wsUserOrdersMiddleware)
+    getDefaultMiddleware().concat(wsUserOrdersMiddleware),
+  preloadedState: persistedState
+});
+
+store.subscribe(() => {
+  saveState(store.getState());
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
